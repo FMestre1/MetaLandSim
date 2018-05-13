3e4cf8703980517a9f98298984df2ba31ec044d0
FMestre1 <mestre.frederico@gmail.com>
2018-05-13 23:57:59 +0100

    updating plot_graph

--- R/plot_graph.R
+++ R/plot_graph.R
@@ -44,7 +44,7 @@ function (rl, species, links)
             cores[i] <- ifelse(dframe$species[i] == 0,"red","green")
           }
         #plot(dframe[,1], dframe[,2], xlim=c(min(dframe[,1]),min(dframe[,1])+mapsize), ylim=c(min(dframe[,2]),min(dframe[,2])+mapsize),
-        #     pch=20, xlab="X", ylab="Y", col=cores)
+        #     pch=20, xlab="X", ylab="Y", col=cores)#deleted
         circ <- symbols(x=dframe[, 1], y=dframe[, 2], circles = dframe[, 4],
                         bg=cores, fg=cores, inches = FALSE)      }
     if(nrow(rl$nodes.characteristics) > 1)
